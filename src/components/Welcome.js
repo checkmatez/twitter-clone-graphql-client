74c3f0aba4000dfdc94f4c33b5e54d8dde6c4e82
Checkmatez <checkmatez@MacBook-Davidov.local>
2017-09-15 00:50:53 +0300

    initial setup

---
+++ src/components/Welcome.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+// prettier-ignore
+const Root = styled.View`
+  alignItems: center;
+  justifyContent: center;
+  flex: 1;
+  backgroundColor: ${props => props.theme.WHITE};
+  width: 90%;
+  alignSelf: center;
+`
+
+// prettier-ignore
+const Text = styled.Text`
+  color: ${props => props.theme.PRIMARY};
+  fontSize: 18;
+  textAlign: center;
+`
+
+export default function Welcome() {
+  return (
+    <Root>
+      <Text>Welcome, if you see this that mean everything work!!!</Text>
+    </Root>
+  )
+}
